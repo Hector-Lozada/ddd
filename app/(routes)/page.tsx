d30297f8f25e21b32e608682b6512dd6cf7b434b
Hector-Lozada <eeaae7570@gmail.com>
2024-07-16 21:01:42 -0500

    router

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -5,8 +5,13 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
       <UserButton/>
+      <h2 className="text-2xl mb-4">Track Record</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
+        <div>Card Summary</div>
+        <div>Card Summary</div>
+        <div>Card Summary</div>
+      </div>
     </div>
   )
 }
