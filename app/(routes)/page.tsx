59e1b7e729826c90f0d53ccaeb27a8ba4b9b5ccc
Hector-Lozada <eeaae7570@gmail.com>
2024-07-16 22:04:42 -0500

    ss

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,7 +2,29 @@ import { UserButton } from "@clerk/nextjs";
 import { CardSummary } from "./components/CardSummary";
 import { BookOpenCheck, DollarSign, LineChart, Percent, UserRound, Waypoints } from "lucide-react";
 
-
+export const dataCardsSummary = [
+  {
+    icon: DollarSign ,
+    total: "$ 215,500.23",
+    title: "Capital",
+    average: 100,
+    tooltipText: "Capital total",
+  },
+  {
+    icon: LineChart,
+    total: " $15,500.23",
+    title: "P&L",
+    average: 100,
+    tooltipText: "Profit",
+  },
+  {
+    icon: Percent,
+    total: "100%",
+    title: "Winrate",
+    average: 100,
+    tooltipText: "Efectividad de la estrategia",
+  },
+]
 
 export default function Home() {
   return (
@@ -10,27 +32,16 @@ export default function Home() {
       <UserButton/>
       <h2 className="text-2xl mb-4">Track Record</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-x-20">
-        <CardSummary
-          icon={DollarSign}
-          total="$ 215,000.00"
-          title="Capital"
-          average={15}
-          tooltipText="Capital total"
-        />
-        <CardSummary
-          icon={LineChart}
-          total="$ 15,000"
-          average={100}
-          title="P&L"
-          tooltipText="Profit"
-        />
-        <CardSummary
-          icon={Percent}
-          total="100%"
-          average={100}
-          title="Winrate"
-          tooltipText="Efectividad de la metodología"
-        />
+        {dataCardsSummary.map(({icon, total, title, average, tooltipText}) => (
+          <CardSummary 
+            key={title}
+            icon={icon}
+            total={total}
+            title={title}
+            average={average}
+            tooltipText={tooltipText}
+          />
+        ))}
       </div>
     </div>
   )
