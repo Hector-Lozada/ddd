1a72cb37197e8437dcfe0891fc91fb709668d9ee
Héctor-Lozada <eeaae7570@gmail.com>
2024-07-25 15:10:29 -0500

    moodify

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -8,6 +8,10 @@ export default function LayoutDashboard({children}: {children:React.ReactElement
             SideBar
         </div>
         <div className='w-full xl:ml-80'>
+            <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4" role="alert">
+              <p className="font-bold">Aviso</p>
+              <p>Esta página está actualmente en desarrollo. Algunas funciones pueden no estar disponibles o pueden cambiar en el futuro.</p>
+            </div>
             <NavBar/>
             <div className='p-6 bg-[#fafbfc] dark:bg-secondary'>
                 {children}
